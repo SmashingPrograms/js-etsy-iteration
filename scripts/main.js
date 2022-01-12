50f191303daa14b34c862a4e16dee08c25040b83
SmashingPrograms <smashingprograms@gmail.com>
2022-01-11 22:08:37 -0500

    Finish assignment

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+// 1. Show me how to calculate the average price of all items. Please console.log the average.
+
+const sumOfPrices = items.reduce(function(total, item) {
+  return total + item.price;
+}, 0);
+
+const averageOfPrices = sumOfPrices / items.length;
+
+console.log(averageOfPrices);
+
+// 2. Show me how to get an array of items that cost between $14.00 and $18.00 USD. Please console.log the answer.
+
+const filterPrices = items.filter(function(item) {
+  return item.price >= 14 && item.price <=18 && item["currency_code"] === 'USD';
+})
+
+console.log(filterPrices);
+
+// 3. Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
+
+const GBPItems = items.filter(function(item) {
+  return item["currency_code"] === 'GBP';
+})
+
+GBPItems.forEach(function(item) {
+  console.log(`${item.title} costs £${item.price}`);
+});
+
+// 4. Show me how to find which items are made of wood. Please console.log the ones you find.
+
+const woodItems = items.filter(function(item) {
+  return item.materials.includes('wood');
+});
+
+console.log(woodItems);
+
+// 5. Show me how to find which items are made of eight or more materials. Please console.log the ones you find.
+
+const eightOrMore = items.filter(function(item) {
+  return item.materials.length >= 8;
+});
+
+console.log(eightOrMore);
+
+// 6. Show me how to calculate how many items were made by their sellers. Please console.log your answer.
+
+const madeBySellers = items.filter(function(item) {
+  return item.who_made === 'i_did';
+});
+
+console.log(`${madeBySellers.length} were made by their sellers.`)
